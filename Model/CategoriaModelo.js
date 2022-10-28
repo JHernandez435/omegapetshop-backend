8528ed0fa42349116644451b3ab94a06b395d215
Jonathan Hernandez <69222698+JHernandez435@users.noreply.github.com>
2022-10-28 08:08:53 -0500

    .

--- Model/CategoriaModelo.js
+++ Model/CategoriaModelo.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const CategoriaSchema = mongoose.Schema({
-    nombre: {type: String, maxlength: 40, required: true,unique: true},
-    tipoProducto: {type: String, maxlength: 40, required: true,unique: true},
-    categoria: {type: String, maxlength: 40, required: true,unique: true},
-    tipo: {type: String, maxlength: 100, required: true},
-    edad: {type: String, maxlength: 100, required: true},
-    marca: {type: String, maxlength: 100, required: true},
-    cantKilos: {type: String, maxlength: 100, required: true},
-    precio: {type: Number, maxlength: 100, required: true},
+    nombre: { type: String, maxlength: 100, required: true, },
+    tipoProducto: { type: String, maxlength: 100, required: true,},
+    categoria: { type: String, maxlength: 100, required: true,},
+    enfoque: { type: String, maxlength: 100, required: true },
+    edad: { type: String, maxlength: 100, required: true },
+    marca: { type: String, maxlength: 100, required: true },
+    cantKilos: { type: String, maxlength: 100, required: true },
+    precio: { type: Number, maxlength: 100, required: true },
 });
 
 module.exports = mongoose.model("categorias", CategoriaSchema);
